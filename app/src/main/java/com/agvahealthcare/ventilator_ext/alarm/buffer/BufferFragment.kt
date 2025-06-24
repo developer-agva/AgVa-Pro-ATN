d8fc6db8589f18c54e1bb12b2e42902723be6089
Masoom Zaid <masoom@agvahealthtech.com>
2025-06-24 12:20:05 +0530

    E: Added Reset Alarm Functionality

--- app/src/main/java/com/agvahealthcare/ventilator_ext/alarm/buffer/BufferFragment.kt
+++ app/src/main/java/com/agvahealthcare/ventilator_ext/alarm/buffer/BufferFragment.kt
@@ -12,7 +12,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.agvahealthcare.ventilator_ext.R
 import com.agvahealthcare.ventilator_ext.dashboard.BufferAlarmRecyclerAdapter
+import com.agvahealthcare.ventilator_ext.dashboard.DashBoardActivity
 import com.agvahealthcare.ventilator_ext.dashboard.DashBoardViewModel
+import com.agvahealthcare.ventilator_ext.dashboard.notifyObserver
 import com.agvahealthcare.ventilator_ext.model.AlarmModel
 import kotlinx.android.synthetic.main.content_button_layout.view.*
 import kotlinx.android.synthetic.main.fragment_buffer.*
@@ -58,29 +60,14 @@ class BufferFragment : Fragment() {
                 adapter = bufferAdapter
             }
         }
-        /*dashBoardViewModel?.arrayListTemp?.observe(viewLifecycleOwner) {
-            bufferAdapter = BufferAlarmRecyclerAdapter(ArrayList(it))
-            rvAlarms.apply {
-                layoutManager = LinearLayoutManager(requireContext())
-                adapter = bufferAdapter
-            }
-        }*/
     }
 
     private fun setupClickListener() {
-        //includeButtonDefaults.buttonView.text = getString(R.string.hint_auto)
         includeButtonReset?.buttonView?.setOnClickListener {
-//            notifyBufferAlarmAdapter()
             dashBoardViewModel?.alarms?.value?.clear()
-            notifyBufferAlarmAdapter()
+            (requireActivity() as DashBoardActivity).clearAlarmMap()
+            dashBoardViewModel?.alarms?.notifyObserver()
         }
     }
-
-    private fun updateList(ackList: ArrayList<AlarmModel>){
-        this.ackList = ackList
-        notifyBufferAlarmAdapter()
-    }
-
-    fun notifyBufferAlarmAdapter() = bufferAdapter?.notifyDataSetChanged()
 
 }
